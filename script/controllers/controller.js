77d9e0111a760ed27f63689e9ccd7982402557aa
Tran Thanh Nam <trannam0518@gmail.com>
2017-01-09 13:23:16 +0700

    update_Login_taikhoan

--- script/controllers/controller.js
+++ script/controllers/controller.js
@@ -1,6 +1,6 @@
 
-app.controller("myCtrl1",function($scope){
-	/*var userdata=[
+app.controller("myCtrl1",function($scope, $window){
+	var userdata=[
 	{
 		username:'thanhnam',
 		password:'nam123'
@@ -8,11 +8,15 @@ app.controller("myCtrl1",function($scope){
 	{
 		username:'quoctu',
 		password:'tu123'
-	}];*/
+	}];
 	$scope.login = function(){
-	if($scope.user=='admin' && $scope.pass=='123'){
-		alert('thanh cong');
-	}else alert('that bai')
-	};
+		angular.forEach(userdata, function(value, key){
+		  	if($scope.user==value.username&& $scope.pass==value.password){			
+					var url =("index.html");
+					$window.location.href=url;
+				};									 
+			});
+		
 
+	  };
 });
