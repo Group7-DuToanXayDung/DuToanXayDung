6183796ad7b95236c94af90da964b12bbff5fc72
Le Quoc Tu <it.lequoctu@gmail.com>
2017-01-18 13:43:49 +0700

    xoa het cua Vu

--- js/js-style.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-$(function() {
-
-    $('#login-form-link').click(function(e) {
-		$("#login-form").delay(100).fadeIn(100);
-		$(this).addClass('active');
-		e.preventDefault();
-	});
-});
