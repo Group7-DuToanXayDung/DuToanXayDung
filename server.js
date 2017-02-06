1e670e0ce750827aa3582326276fce1cb41a2740
Tamhuynh <Tamhuynh2489@gmail.com>
2017-02-06 13:21:50 +0700

    Signed-off-by: Tamhuynh <Tamhuynh2489@gmail.com>

--- server.js
+++ server.js
@@ -15,20 +15,20 @@
 
 // require our dependencies
 var express        = require('express');
-var expressLayouts = require('express-ejs-layouts');
-var bodyParser     = require('body-parser');
+// var expressLayouts = require('express-ejs-layouts');
+// var bodyParser     = require('body-parser');
 var app            = express();
-var port           = process.env.PORT || 8080;
+var port           = 8080;
 
 // use ejs and express layouts
-app.set('view engine', 'ejs');
-app.use(expressLayouts);
+// app.set('view engine', 'ejs');
+// app.use(expressLayouts);
 
 // use body parser
-app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.urlencoded({ extended: true }));
 
 // route our app
-var router = require('./app/routes');
+var router = require('router');
 app.use('/', router);
 
 
